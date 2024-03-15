5ded186fe46f2c7ea9f9decb0498d63d183c9f62
gabrielli !! <108476555+lihyyh@users.noreply.github.com>
2024-03-15 12:05:03 -0300

    gg

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ for(let j=0; j<botoes.length; j++){
 botoes[j].classList.remove("ativo");
 textos[j].classList.remove("ativo");
 }
-}
+
 
 botoes[i].classList.add("ativo");
 textos[i].classList.add("ativo");
