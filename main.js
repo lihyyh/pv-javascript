68f50481f1091d845422ab88dc62f91711697d14
gabrielli !! <108476555+lihyyh@users.noreply.github.com>
2024-04-05 11:09:35 -0300

    oi

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+
 for(let i=0;i < botoes.length;i++){
 botoes[i].onclick = function(){
 
@@ -12,10 +13,10 @@ textos[j].classList.remove("ativo");
 botoes[i].classList.add("ativo");
 textos[i].classList.add("ativo");
 }
+}
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 let tempoAtual = new Date();
 contadores[0] .textContent = tempoObjetivo1 - tempoAtual;
-
 
